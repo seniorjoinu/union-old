803b53f480c3a2b64b4cb5c923f07af1f47c5eff
Александр Втюрин <senior.joinu@gmail.com>
2021-05-24 04:03:44 +0300

    add token history

--- src/voting_token/utils.rs
+++ src/voting_token/utils.rs
@@ -1,4 +1,5 @@
 use ic_cdk::export::candid::{CandidType, Deserialize, Nat, Principal};
+use ic_cdk::trap;
 use std::collections::{HashMap, LinkedList};
 
 #[derive(Clone, Debug, Default, CandidType, Deserialize)]
@@ -13,7 +14,42 @@ pub struct BalanceEntry {
     pub balance: Nat,
 }
 
-pub type VotingTokenHistory = LinkedList<BalanceEntry>;
+pub type VotingTokenHistory = Vec<BalanceEntry>;
+
+fn lookup_history(
+    history: &VotingTokenHistory,
+    begin: usize,
+    end: usize,
+    timestamp: i64,
+) -> Option<usize> {
+    let mid_left = begin + (end - begin) / 2;
+    let mid_right = mid_left + 1;
+
+    let left_timestamp = history.get(mid_left).unwrap().timestamp;
+    let right_timestamp = history.get(mid_right).unwrap().timestamp;
+
+    // if we're in between (left is lower, right is higher) or if we found exact value - return its index
+    if left_timestamp < timestamp && right_timestamp > timestamp {
+        return Some(mid_left);
+    }
+
+    if right_timestamp == timestamp {
+        return Some(mid_right);
+    }
+
+    // if we're higher than both left and right, repeat for the left side
+    if left_timestamp < timestamp && right_timestamp < timestamp {
+        return lookup_history(history, mid_right, end, timestamp);
+    }
+
+    // if we're lower than both left and right, repeat for the right side
+    if left_timestamp > timestamp && right_timestamp > timestamp {
+        return lookup_history(history, begin, mid_left, timestamp);
+    }
+
+    // it is impossible, because we checked boundaries before
+    None
+}
 
 #[derive(Clone, Debug, CandidType, Deserialize)]
 pub enum Error {
@@ -38,7 +74,7 @@ impl VotingToken {
     fn peek_balance(&self, token_holder: &Principal) -> Nat {
         match self.balances.get(token_holder) {
             None => Nat::from(0),
-            Some(h) => match h.front() {
+            Some(h) => match h.last() {
                 None => Nat::from(0),
                 Some(b) => b.balance.clone(),
             },
@@ -49,12 +85,43 @@ impl VotingToken {
         match self.balances.get_mut(token_holder) {
             None => {
                 let mut history = VotingTokenHistory::new();
-                history.push_front(entry);
+                history.push(entry);
 
                 self.balances.insert(token_holder.clone(), history);
             }
-            Some(h) => h.push_front(entry),
+            Some(h) => h.push(entry),
         };
+    }
+
+    fn lookup_balance(&self, token_holder: &Principal, timestamp: i64) -> Option<Nat> {
+        match self.balances.get(token_holder) {
+            // if there is no history for account - it's balance is 0
+            None => Some(Nat::from(0)),
+
+            Some(history) => match history.first() {
+                None => Some(Nat::from(0)),
+                Some(f) => {
+                    // if the timestamp is earlier than the first balance snapshot - it's balance is 0
+                    if timestamp < f.timestamp {
+                        return Some(Nat::from(0));
+                    }
+
+                    match history.last() {
+                        None => Some(Nat::from(0)),
+                        Some(l) => {
+                            // if the timestamp is more recent than the last balance snapshot - it's balance is the last balance
+                            if timestamp >= l.timestamp {
+                                return Some(l.balance.clone());
+                            }
+
+                            // otherwise - binary search based lookup
+                            let idx = lookup_history(history, 0, history.len(), timestamp);
+                            idx.map(|i| history.get(i).unwrap().balance.clone())
+                        }
+                    }
+                }
+            },
+        }
     }
 }
 
@@ -117,8 +184,12 @@ impl IVotingToken for VotingToken {
     }
 
     fn balance_of(&self, token_holder: &Principal, timestamp: Option<i64>) -> Nat {
-        // TODO: binary search
-
-        self.peek_balance(token_holder)
+        match timestamp {
+            None => self.peek_balance(token_holder),
+            Some(t) => match self.lookup_balance(token_holder, t) {
+                None => trap("Balance history lookup failed due to internal error"),
+                Some(b) => b,
+            },
+        }
     }
 }
